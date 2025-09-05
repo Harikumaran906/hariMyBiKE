eb427a52566108901938e244a9381942aacc503a
Harikumaran906 <kumaranh906@gmail.com>
2025-09-05 11:06:28 +0530

    final

--- scrip.js
+++ scrip.js
@@ -6,3 +6,25 @@ phoneBtn.addEventListener("click", function() {
     phoneNumHead.classList.toggle("hide")
 });
 
+// Menu panel toggle
+const menuPanel = document.querySelector(".menu-panel");
+const closeBtn = document.querySelector(".close-btn");
+
+menuBtn.addEventListener("click", function() {
+  menuPanel.classList.add("is-open");
+});
+
+closeBtn.addEventListener("click", function(e) {
+  e.preventDefault();
+  menuPanel.classList.remove("is-open");
+});
+
+document.querySelectorAll('.nav a[role="menuitem"]').forEach(link => {
+  link.addEventListener('click', () => {
+    menuPanel.classList.remove('is-open');
+  });
+});
+
+window.addEventListener('hashchange', () => {
+  menuPanel.classList.remove('is-open');
+});
